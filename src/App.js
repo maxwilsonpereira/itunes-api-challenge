671109a9ad0cc83ace491d0109d1b7f91293dc71
maxwilsonpereira <maxwilsonpereira@gmail.com>
2021-10-15 23:48:37 +0200

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import classes from './app.styles.module.css';
 
 function App() {
+  const [songs, setSongs] = useState(['A', 'B', 'C', 'D', 'E']);
+  const [albuns, setAlbuns] = useState([]);
+  const [searchString, setSearchString] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSongs([]);
+      setSongs(updateSongsHandler(albuns));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [albuns]);
+
+  const updateSongsHandler = () => {
+    let aux = [];
+    if (albuns.length > 0) aux = albuns;
+    else aux = songs;
+    aux.push(aux.shift());
+    return aux;
+  };
+
+  useEffect(() => {
+    if (searchString.length > 2) {
+      fetch(`https://itunes.apple.com/search?term=${searchString}`)
+        .then((res) => {
+          res.json().then(({ results }) => {
+            console.log('results: ', results);
+            const albuns = results
+              .sort((a, b) => (a.collectionName < b.collectionName ? -1 : 1))
+              .map((item) => {
+                return item.collectionName;
+              });
+            setAlbuns([albuns[0], albuns[1], albuns[2], albuns[3], albuns[4]]);
+          });
+        })
+        .catch((err) => {
+          console.log('ERROR: ', err);
+        });
+    }
+  }, [searchString]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <input
+        className={classes.inputClass}
+        type="text"
+        placeholder="Search Band"
+        onChange={(e) => setSearchString(e.target.value)}
+      />
+      <div className={classes.container}>
+        {songs.map((item, index) => (
+          <div key={item + index} className={classes.itemContainer}>
+            {item}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
